c481a66e70266aea3073bc361e6b7bce1f616cb2
Gabrielle Duarte <gabi.duarte@gmail.com>
2016-04-01 00:19:55 -0300

    Added view and controller basic methods

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,13 @@
 class TodosController < ApplicationController
 	
 	def index
+		@todos = Todo.all
+
+		# serves as todos.json as well
+		respond_to do |format|
+			format.html
+			format.json { render json: @todos }
+		end
 	end
 
 end
