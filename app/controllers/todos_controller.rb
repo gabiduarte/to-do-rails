1c77b21f88c542d7fed9d8e63641d8d22fef2f07
Gabrielle Duarte <gabi.duarte@gmail.com>
2016-04-06 00:35:02 -0300

    Create action w/ form and flash notice

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,6 +3,8 @@ class TodosController < ApplicationController
 	def index
 		@todos = Todo.all
 
+    @todo = Todo.new
+
 		# serves as todos.json as well
 		respond_to do |format|
 			format.html
@@ -18,5 +20,29 @@ class TodosController < ApplicationController
 		# 	format.json { render json: @todo }
 		# end
 	end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+
+    if @todo.save
+      flash[:notice] = "Todo salvo"
+      redirect_to(todos_path)
+    else
+      flash[:notice] = "Houve um erro"
+      render :new
+    end
+  end
+
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:name, :completed)
+  end
+
 
 end
